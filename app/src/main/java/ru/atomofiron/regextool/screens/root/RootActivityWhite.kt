2d7cd28f836111cd3107c9f053181344c76c3894
atomofiron <atomofiron@gmail.com>
2020-01-07 04:17:17 +0300

    nameless commit

--- app/src/main/java/ru/atomofiron/regextool/screens/root/RootActivityWhite.kt
+++ app/src/main/java/ru/atomofiron/regextool/screens/root/RootActivityWhite.kt
@@ -1,16 +1,24 @@
 package ru.atomofiron.regextool.screens.root
 
 import android.os.Bundle
+import android.view.View
 import ru.atomofiron.regextool.R
 import ru.atomofiron.regextool.common.base.BaseActivity
+import ru.atomofiron.regextool.common.util.Knife
 import kotlin.reflect.KClass
 
 open class RootActivityWhite : BaseActivity<RootViewModel>() {
 
     override val viewModelClass: KClass<RootViewModel> = RootViewModel::class
 
+    private val joystick = Knife<View>(this, R.id.root_iv_joystick)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         setContentView(R.layout.activity_root)
         super.onCreate(savedInstanceState)
+
+        joystick.view.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
