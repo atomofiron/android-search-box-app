eedd27ba971d042d898e6291dbbc3cc4564a2fd1
atomofiron <atomofiron@gmail.com>
2020-02-19 19:25:24 +0300

    nameless commit

--- app/src/main/java/ru/atomofiron/regextool/screens/root/RootActivityWhite.kt
+++ app/src/main/java/ru/atomofiron/regextool/screens/root/RootActivityWhite.kt
@@ -3,11 +3,11 @@ package ru.atomofiron.regextool.screens.root
 import android.os.Bundle
 import android.view.View
 import androidx.core.content.ContextCompat
-import com.google.android.material.snackbar.BaseTransientBottomBar
 import com.google.android.material.snackbar.Snackbar
 import ru.atomofiron.regextool.R
 import ru.atomofiron.regextool.common.base.BaseActivity
 import ru.atomofiron.regextool.common.util.Knife
+import ru.atomofiron.regextool.screens.root.util.ExitSnackbarCallback
 import kotlin.reflect.KClass
 
 open class RootActivityWhite : BaseActivity<RootViewModel>() {
@@ -34,16 +34,7 @@ open class RootActivityWhite : BaseActivity<RootViewModel>() {
                 .setAnchorView(anchor.view)
                 .setActionTextColor(ContextCompat.getColor(this, R.color.colorAccent))
                 .setAction(R.string.exit) { viewModel.onExitClick() }
-                .addCallback(ExitSnackbarCallback())
-    }
-
-    private inner class ExitSnackbarCallback : BaseTransientBottomBar.BaseCallback<Snackbar>() {
-        override fun onShown(transientBottomBar: Snackbar?) {
-            viewModel.sbExitIsShown = true
-        }
-        override fun onDismissed(transientBottomBar: Snackbar?, event: Int) {
-            viewModel.sbExitIsShown = false
-        }
+                .addCallback(ExitSnackbarCallback(viewModel))
     }
 
     // todo onNewIntent ACTION_SHOW_RESULT, ACTION_SHOW_RESULTS
