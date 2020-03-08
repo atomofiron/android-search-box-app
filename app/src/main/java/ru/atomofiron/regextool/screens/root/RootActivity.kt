579d2090060068b934fbcbed94f3c88b9824550c
atomofiron <atomofiron@gmail.com>
2020-03-08 15:16:42 +0300

    nameless commit

--- app/src/main/java/ru/atomofiron/regextool/screens/root/RootActivity.kt
+++ app/src/main/java/ru/atomofiron/regextool/screens/root/RootActivity.kt
@@ -5,12 +5,14 @@ import android.view.View
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
-import com.google.android.material.snackbar.Snackbar
-import ru.atomofiron.regextool.R
 import app.atomofiron.common.base.BaseActivity
 import app.atomofiron.common.util.Knife
+import app.atomofiron.common.util.LazyReincarnation
+import com.google.android.material.snackbar.Snackbar
+import ru.atomofiron.regextool.R
 import ru.atomofiron.regextool.model.AppOrientation
 import ru.atomofiron.regextool.screens.root.util.ExitSnackbarCallback
+import ru.atomofiron.regextool.view.custom.Joystick
 import kotlin.reflect.KClass
 
 open class RootActivity : BaseActivity<RootViewModel>() {
@@ -18,9 +20,16 @@ open class RootActivity : BaseActivity<RootViewModel>() {
     override val viewModelClass: KClass<RootViewModel> = RootViewModel::class
 
     private val root = Knife<CoordinatorLayout>(this, R.id.root_cl_root)
-    private val joystick = Knife<View>(this, R.id.root_iv_joystick)
+    private val joystick = Knife<Joystick>(this, R.id.root_iv_joystick)
     private val anchor = Knife<View>(this, R.id.root_v_anchor)
-    private lateinit var sbExit: Snackbar
+
+    private val sbExit: LazyReincarnation<Snackbar> = LazyReincarnation {
+        Snackbar.make(joystick.view, R.string.click_back_to_exit, Snackbar.LENGTH_SHORT)
+                .setAnchorView(anchor.view)
+                .setActionTextColor(ContextCompat.getColor(this@RootActivity, R.color.colorAccent))
+                .setAction(R.string.exit) { viewModel.onExitClick() }
+                .addCallback(ExitSnackbarCallback(viewModel))
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setContentView(R.layout.activity_root)
@@ -31,14 +40,13 @@ open class RootActivity : BaseActivity<RootViewModel>() {
         }
 
         viewModel.showExitSnackbar.observeEvent(this) {
-            sbExit.show()
+            sbExit { show() }
         }
+    }
 
-        sbExit = Snackbar.make(joystick.view, R.string.click_back_to_exit, Snackbar.LENGTH_SHORT)
-                .setAnchorView(anchor.view)
-                .setActionTextColor(ContextCompat.getColor(this, R.color.colorAccent))
-                .setAction(R.string.exit) { viewModel.onExitClick() }
-                .addCallback(ExitSnackbarCallback(viewModel))
+    override fun setTheme(resId: Int) {
+        super.setTheme(resId)
+        sbExit.wipe()
     }
 
     override fun onSubscribeData(owner: LifecycleOwner) {
