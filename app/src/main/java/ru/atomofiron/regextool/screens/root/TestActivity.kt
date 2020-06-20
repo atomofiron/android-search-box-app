6e9a127a08f14e7cec215ac486f29ee73c21699f
atomofiron <atomofiron@gmail.com>
2020-06-20 15:03:22 +0300

    useless commit

---
+++ app/src/main/java/ru/atomofiron/regextool/screens/root/TestActivity.kt
@@ -0,0 +1,13 @@
+package ru.atomofiron.regextool.screens.root
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.atomofiron.regextool.R
+
+class TestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
+}
