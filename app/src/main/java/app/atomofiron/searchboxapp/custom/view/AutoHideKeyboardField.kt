33325357ce2b55f0b7e17ac06cd262f8fac66115
atomofiron <atomofiron@gmail.com>
2022-11-25 00:05:35 +0400

    add flagNoExtractUi to AutoHideKeyboardField

--- app/src/main/java/app/atomofiron/searchboxapp/custom/view/AutoHideKeyboardField.kt
+++ app/src/main/java/app/atomofiron/searchboxapp/custom/view/AutoHideKeyboardField.kt
@@ -5,18 +5,19 @@ import android.content.res.Configuration
 import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.widget.AppCompatEditText
 import app.atomofiron.searchboxapp.R
 
 open class AutoHideKeyboardField : AppCompatEditText {
-    private var inputMethodManager: InputMethodManager? = null
+    private val inputMethodManager: InputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
 
     protected var hideKeyboardOnFocusLost: Boolean = true
     protected var hideKeyboardOnDetached: Boolean = true
 
-    @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) : super(context, attrs)
-
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet? = null) : super(context, attrs)
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
         val styled = context.obtainStyledAttributes(attrs, R.styleable.AutoHideKeyboardField, defStyleAttr, 0)
         hideKeyboardOnFocusLost = styled.getBoolean(R.styleable.AutoHideKeyboardField_hideKeyboardOnFocusLost, hideKeyboardOnFocusLost)
@@ -24,17 +25,18 @@ open class AutoHideKeyboardField : AppCompatEditText {
     }
 
     init {
-        inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imeOptions = imeOptions or EditorInfo.IME_FLAG_NO_EXTRACT_UI or EditorInfo.IME_FLAG_NO_FULLSCREEN
     }
 
     override fun onKeyPreIme(keyCode: Int, event: KeyEvent): Boolean {
+        val result = super.onKeyPreIme(keyCode, event)
         if ((keyCode == KeyEvent.KEYCODE_ESCAPE || keyCode == KeyEvent.KEYCODE_BACK) && hasFocus()) {
             clearFocus()
-            inputMethodManager!!.hideSoftInputFromWindow(windowToken, 0)
+            inputMethodManager.hideSoftInputFromWindow(windowToken, 0)
             // перехватываем событие, чтобы оно не обработалось при уже закрытой клавиатуре
             return true
         }
-        return false
+        return result
     }
 
     override fun onFocusChanged(focused: Boolean, direction: Int, previouslyFocusedRect: Rect?) {
@@ -43,9 +45,9 @@ open class AutoHideKeyboardField : AppCompatEditText {
         when {
             !hideKeyboardOnFocusLost -> Unit
             keyboard == Configuration.KEYBOARD_QWERTY -> Unit
-            !focused -> inputMethodManager!!.hideSoftInputFromWindow(windowToken, 0)
+            !focused -> inputMethodManager.hideSoftInputFromWindow(windowToken, 0)
             else -> {
-                inputMethodManager!!.showSoftInput(this, 0)
+                inputMethodManager.showSoftInput(this, 0)
                 setSelection(length())
             }
         }
@@ -54,7 +56,7 @@ open class AutoHideKeyboardField : AppCompatEditText {
     override fun onDetachedFromWindow() {
         super.onDetachedFromWindow()
         if (hideKeyboardOnDetached) {
-            inputMethodManager!!.hideSoftInputFromWindow(windowToken, 0)
+            inputMethodManager.hideSoftInputFromWindow(windowToken, 0)
         }
     }
 }
