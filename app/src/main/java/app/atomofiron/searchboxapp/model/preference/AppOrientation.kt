7e0edcdd385b8c75c8ea2697bd4fa7873fc2721a
atomofiron <atomofiron@gmail.com>
2022-11-24 19:38:17 +0400

    add orientation option by sensor

--- app/src/main/java/app/atomofiron/searchboxapp/model/preference/AppOrientation.kt
+++ app/src/main/java/app/atomofiron/searchboxapp/model/preference/AppOrientation.kt
@@ -5,5 +5,6 @@ import android.content.pm.ActivityInfo
 enum class AppOrientation(val constant: Int) {
     UNDEFINED(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED),
     PORTRAIT(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT),
-    LANDSCAPE(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    LANDSCAPE(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE),
+    SENSOR(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
 }
