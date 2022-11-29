e10e8fe8d9ebccbe8a34261e8eca9bc4523c2931
atomofiron <atomofiron@gmail.com>
2022-11-30 03:31:53 +0400

    improve explorer items frame border decorator

--- app/src/main/java/app/atomofiron/searchboxapp/screens/explorer/adapter/ItemBorderDecorator.kt
+++ app/src/main/java/app/atomofiron/searchboxapp/screens/explorer/adapter/ItemBorderDecorator.kt
@@ -1,6 +1,7 @@
 package app.atomofiron.searchboxapp.screens.explorer.adapter
 
 import android.graphics.*
+import android.graphics.Path.Direction
 import android.view.View
 import androidx.core.view.iterator
 import androidx.recyclerview.widget.RecyclerView
@@ -17,12 +18,21 @@ class ItemBorderDecorator(
     private val headerView: ExplorerHeaderView,
 ) : ItemDecoration() {
 
-    private var grey = headerView.context.findColorByAttr(R.attr.colorOutline)
-    private var accent = headerView.context.findColorByAttr(R.attr.colorTertiary)
-    private var cornerRadius = headerView.resources.getDimension(R.dimen.explorer_border_corner_radius)
-    private var borderWidth = headerView.resources.getDimension(R.dimen.explorer_border_width)
-    private var levelSpace = headerView.resources.getDimension(R.dimen.explorer_level_space)
-    private var innerPadding = headerView.resources.getDimension(R.dimen.content_margin)
+    private val items get() = adapter.currentList
+    private val grey = headerView.context.findColorByAttr(R.attr.colorOutline)
+    private val accent = headerView.context.findColorByAttr(R.attr.colorTertiary)
+    private val innerPadding = headerView.resources.getDimension(R.dimen.content_margin)
+    private val cornerRadius = headerView.resources.getDimension(R.dimen.explorer_border_corner_radius)
+    private val borderWidth = headerView.resources.getDimension(R.dimen.explorer_border_width)
+    // под открытой пустой директорией
+    private val emptySpace = cornerRadius * 2
+    // под открытой не пустой директорией
+    private val openedSpace = cornerRadius
+    // под последним айтемом глубочайшей директории
+    private val openedEndSpace = cornerRadius * 2
+    // расстояние между низом последнего айтема глубочайшей директории и нижним краем рамки,
+    // а так же минимальное расстояние между низом открытой директории и нижним краем рамки
+    private val frameBottomOffset = openedEndSpace / 2 + borderWidth / 2
 
     private var currentDir: Node? = null
     private val paint = Paint()
@@ -41,90 +51,92 @@ class ItemBorderDecorator(
     }
 
     override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
-
         val holder = parent.getChildViewHolder(view)
         holder ?: return
-        val item = adapter.currentList[holder.bindingAdapterPosition]
-        val next = adapter.currentList.getOrNull(holder.bindingAdapterPosition.inc())
-        when {
-            item.isOpened && item.isEmpty -> outRect.bottom = levelSpace.toInt() * 2
-            item.parentPath != next?.parentPath -> outRect.bottom = levelSpace.toInt()
-        }
+        val item = items[holder.bindingAdapterPosition]
+        val next = items.getOrNull(holder.bindingAdapterPosition.inc())
+        outRect.bottom = when {
+            item.isOpened && item.isEmpty -> emptySpace
+            item.isOpened -> openedSpace
+            item.parentPath != next?.parentPath -> openedEndSpace
+            else -> return
+        }.toInt()
     }
 
     override fun onDrawOver(canvas: Canvas, parent: RecyclerView, state: RecyclerView.State) {
         val first = parent.getChildAt(0)
         first ?: return
-        val items = adapter.currentList
         val firstIndex = parent.getChildViewHolder(first).bindingAdapterPosition
         val lastIndex = firstIndex + parent.childCount.dec()
         var currentIndex = firstIndex
         var frameRect: RectF? = null
-        var currentDirMinTop = headerView.height.toFloat()
+        val left = parent.paddingLeft.toFloat()
+        val right = parent.width - parent.paddingRight.toFloat()
+        val headerBottom = headerView.height.toFloat()
+        val parentBottom = parent.height.toFloat() - parent.paddingBottom
         paint.color = grey
         for (child in parent) {
-            var left = parent.paddingLeft.toFloat()
-            var right = parent.width - parent.paddingRight.toFloat()
             val prev = if (currentIndex == firstIndex) null else items.getOrNull(currentIndex.dec())
             val item = items[currentIndex]
             val next = if (currentIndex == lastIndex) null else items.getOrNull(currentIndex.inc())
-            val nextParentPath = next?.parentPath ?: ""
-            val childBottomOffset = child.bottom + levelSpace / 2
             when {
+                // под открытой пустой папкой всё просто
                 item.isOpened && item.isEmpty -> {
-                    val top = child.bottom.toFloat()
-                    val bottom = child.bottom + levelSpace * 2
-                    rect.set(left, top, right, bottom)
                     frameRect = rect
-                    currentDirMinTop = -cornerRadius
+                    val top = child.bottom.toFloat()
+                    val bottom = child.bottom + emptySpace
+                    rect.set(left, top, right, bottom)
                 }
-                item.parentPath == currentDir?.path -> {
+                // под глубочайшей открытой директорией задаём с рассчётом на то,
+                // что дочерние айтемы может быть не видно
+                item.isOpened && item.path == currentDir?.path -> {
+                    frameRect = rect
                     rect.left = left
                     rect.right = right
-                    if (item.parentPath != prev?.parentPath) {
-                        rect.top = child.top - levelSpace
-                    }
-                    if (item.parentPath != next?.parentPath) {
-                        rect.bottom = childBottomOffset + borderWidth / 2
-                    }
-                    frameRect = rect
+                    rect.top = child.bottom.toFloat()
+                    rect.bottom = child.bottom + frameBottomOffset
                 }
-                item.isOpened && item.uniqueId == currentDir?.uniqueId -> Unit
+                item.parentPath == currentDir?.path -> {
+                    frameRect = rect
+                    rect.left = left
+                    rect.right = right
+                    // верхняя граница рамки или у низа хедера текущей директории,
+                    // или у низа айтема текущей директории
+                    if (item.parentPath != prev?.parentPath) {
+                        rect.top = child.top - openedSpace
+                        rect.top = max(rect.top, headerBottom)
+                    }
+                    // top: хедер уезжает вместе с низом последнего айтема текущей директории
+                    // bottom: указываем на нижнюю границу рамки,
+                    // которая не должна быть ниже области видимости,
+                    // но только если айтем текущей директории не оказывается слишком низко,
+                    // чтобы игнорировать область видимости
+                    if (item.parentPath != next?.parentPath) {
+                        rect.top = min(rect.top, child.bottom.toFloat())
+                        rect.bottom = child.bottom + frameBottomOffset
+                        rect.bottom = min(rect.bottom, parentBottom)
+                        rect.bottom = max(rect.bottom, rect.top + frameBottomOffset)
+                    }
+                }
                 !item.isOpened && item.isRoot -> Unit
-                item.parentPath != nextParentPath -> {
-                    left += innerPadding
-                    right -= innerPadding
-                    canvas.drawLine(left, childBottomOffset, right, childBottomOffset, paint)
+                item.parentPath != next?.parentPath -> {
+                    val bottom = child.bottom + openedSpace / 2
+                    canvas.drawLine(left + innerPadding, bottom, right - innerPadding, bottom, paint)
                 }
             }
             currentIndex++
         }
-        frameRect?.let {
-            frameRect.bottom = min(frameRect.bottom, parent.height.toFloat() - parent.paddingBottom)
-            val boxHeight = frameRect.height()
-            if (boxHeight > 0) {
-                paint.color = accent
-                framePath.reset()
-                val minTop = min(currentDirMinTop, frameRect.bottom - levelSpace / 2)
-                framePath.addRoundRect(
-                    frameRect.left,
-                    max(minTop, frameRect.top),
-                    frameRect.right,
-                    frameRect.bottom,
-                    FloatArray(8) { if (it <= 3) 0f else cornerRadius },
-                    Path.Direction.CW,
-                )
-                framePath.addRoundRect(
-                    frameRect.left + borderWidth,
-                    max(minTop, frameRect.top),
-                    frameRect.right - borderWidth,
-                    frameRect.bottom - borderWidth,
-                    cornerRadius - borderWidth,
-                    cornerRadius - borderWidth,
-                    Path.Direction.CCW,
-                )
-                canvas.drawPath(framePath, paint)
-            }
-        }
+        frameRect?.drawFrame(canvas)
+    }
+
+    private fun RectF.drawFrame(canvas: Canvas) {
+        val stroke = borderWidth
+        val innerRadius = cornerRadius - stroke
+        val radii = FloatArray(8) { if (it <= 3) 0f else cornerRadius }
+        paint.color = accent
+        framePath.reset()
+        framePath.addRoundRect(left, top, right, bottom, radii, Direction.CW)
+        framePath.addRoundRect(left + stroke, top, right - stroke, bottom - stroke, innerRadius, innerRadius, Direction.CCW)
+        canvas.drawPath(framePath, paint)
     }
 }
