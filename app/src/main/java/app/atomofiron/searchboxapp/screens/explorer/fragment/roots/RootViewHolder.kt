c1e55807122953e726a4dfe731d9a2fc8ab04288
atomofiron <atomofiron@gmail.com>
2022-12-04 18:17:43 +0400

    add icons for explorer roots

--- app/src/main/java/app/atomofiron/searchboxapp/screens/explorer/fragment/roots/RootViewHolder.kt
+++ app/src/main/java/app/atomofiron/searchboxapp/screens/explorer/fragment/roots/RootViewHolder.kt
@@ -1,11 +1,11 @@
 package app.atomofiron.searchboxapp.screens.explorer.fragment.roots
 
-import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import android.view.View
+import androidx.core.content.ContextCompat
 import app.atomofiron.common.recycler.GeneralHolder
 import app.atomofiron.searchboxapp.R
 import app.atomofiron.searchboxapp.databinding.ItemExplorerCardBinding
-import app.atomofiron.searchboxapp.getColorByAttr
 import app.atomofiron.searchboxapp.model.explorer.NodeRoot
 import app.atomofiron.searchboxapp.model.explorer.NodeRoot.NodeRootType
 
@@ -14,14 +14,15 @@ class RootViewHolder(itemView: View) : GeneralHolder<NodeRoot>(itemView) {
     private val binding = ItemExplorerCardBinding.bind(itemView)
 
     init {
-        val clr = itemView.context.getColorByAttr(R.attr.colorOutline)
-        binding.cartThumbnail.foreground = ColorDrawable(clr)
         binding.cartThumbnail.clipToOutline = true
-        binding.cartThumbnail.setBackgroundResource(R.drawable.clip_card_thumbnail)
     }
 
     override fun onBind(item: NodeRoot, position: Int) {
         binding.cartTitle.text = item.getTitle()
+        when (item.thumbnail) {
+            null -> binding.cartThumbnail.setImageDrawable(item.getIcon())
+            else -> binding.cartThumbnail.setImageBitmap(item.thumbnail)
+        }
     }
 
     private fun NodeRoot.getTitle(): String {
@@ -33,7 +34,21 @@ class RootViewHolder(itemView: View) : GeneralHolder<NodeRoot>(itemView) {
             is NodeRootType.Bluetooth -> context.getString(R.string.root_bluetooth)
             is NodeRootType.Screenshots -> context.getString(R.string.root_screenshots)
             is NodeRootType.InternalStorage -> context.getString(R.string.internal_storage)
-            is NodeRootType.Favorite -> ""
+            is NodeRootType.Favorite -> type.item.name
         }
+    }
+
+    private fun NodeRoot.getIcon(): Drawable? {
+        val resId = when (type) {
+            is NodeRootType.Photos -> R.drawable.ic_thumbnail_camera
+            is NodeRootType.Videos -> R.drawable.ic_thumbnail_videocam
+            is NodeRootType.Camera -> R.drawable.ic_thumbnail_camera
+            is NodeRootType.Downloads -> R.drawable.ic_thumbnail_download
+            is NodeRootType.Bluetooth -> R.drawable.ic_thumbnail_bluetooth
+            is NodeRootType.Screenshots -> R.drawable.ic_thumbnail_screenshot
+            is NodeRootType.InternalStorage -> R.drawable.ic_thumbnail_memory
+            is NodeRootType.Favorite -> R.drawable.ic_thumbnail_favorite
+        }
+        return ContextCompat.getDrawable(context, resId)
     }
 }
