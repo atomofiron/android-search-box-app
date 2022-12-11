8a74653a11ddc2ca90cba5669772345d2ed3d9b0
atomofiron <atomofiron@gmail.com>
2022-12-11 09:38:05 +0400

    set exportSchema = false for @Database

--- app/src/main/java/app/atomofiron/searchboxapp/screens/finder/history/dao/HistoryDatabase.java
+++ app/src/main/java/app/atomofiron/searchboxapp/screens/finder/history/dao/HistoryDatabase.java
@@ -3,7 +3,7 @@ package app.atomofiron.searchboxapp.screens.finder.history.dao;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = { ItemHistory.class }, version = 1)
+@Database(entities = { ItemHistory.class }, version = 1, exportSchema = false)
 public abstract class HistoryDatabase extends RoomDatabase {
    public abstract HistoryDao historyDao();
 }
