4d5dc9fea2b98c6ce3cfe2392f21e6d469e8eaa4
atomofiron <atomofiron@gmail.com>
2023-04-23 20:05:03 +0400

    little bit refactoring in WideRecyclerView

--- app/src/main/java/app/atomofiron/searchboxapp/custom/view/WideRecyclerView.kt
+++ app/src/main/java/app/atomofiron/searchboxapp/custom/view/WideRecyclerView.kt
@@ -14,29 +14,23 @@ class WideRecyclerView : RecyclerView {
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
-    override fun onMeasure(widthSpec: Int, heightSpec: Int) {
-        val parent = parent as View
-        if (paddingStart != parent.paddingStart || paddingEnd != parent.paddingEnd) {
-            update(parent)
-        }
-        val customWidthSpec = MeasureSpec.makeMeasureSpec(parent.width, MeasureSpec.getMode(widthSpec))
-        super.onMeasure(customWidthSpec, heightSpec)
-    }
-
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
         val parent = parent as View
-        if (paddingStart != parent.paddingStart || paddingEnd != parent.paddingEnd) {
-            update(parent)
-        }
+        tryUpdate(parent)
     }
 
     override fun onConfigurationChanged(newConfig: Configuration?) {
         super.onConfigurationChanged(newConfig)
         val parent = parent as View
-        if (paddingStart != parent.paddingStart || paddingEnd != parent.paddingEnd) {
-            update(parent)
-        }
+        tryUpdate(parent)
+    }
+
+    override fun onMeasure(widthSpec: Int, heightSpec: Int) {
+        val parent = parent as View
+        tryUpdate(parent)
+        val customWidthSpec = MeasureSpec.makeMeasureSpec(parent.width, MeasureSpec.getMode(widthSpec))
+        super.onMeasure(customWidthSpec, heightSpec)
     }
 
     override fun onLayout(changed: Boolean, l: Int, top: Int, r: Int, bottom: Int) {
@@ -44,11 +38,13 @@ class WideRecyclerView : RecyclerView {
         super.onLayout(changed, 0, top, parent.width, bottom)
     }
 
-    private fun update(parent: View) {
-        updatePadding(parent.paddingStart, parent.paddingEnd)
-        updateLayoutParams<MarginLayoutParams> {
-            marginStart = -parent.paddingStart
-            marginEnd = -parent.paddingEnd
+    private fun tryUpdate(parent: View) {
+        if (paddingStart != parent.paddingStart || paddingEnd != parent.paddingEnd) {
+            updatePadding(parent.paddingStart, parent.paddingEnd)
+            updateLayoutParams<MarginLayoutParams> {
+                marginStart = -parent.paddingStart
+                marginEnd = -parent.paddingEnd
+            }
         }
     }
 
